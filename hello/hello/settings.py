e5e148f20059f0bd80c9e208f9cc2006c1f209ca
Prince Aman <111490830+Prince-2204@users.noreply.github.com>
2024-06-05 01:07:41 +0530

    Update settings.py

--- hello/hello/settings.py
+++ hello/hello/settings.py
@@ -137,3 +137,15 @@ STATICFILES_DIRS = [
     #"/var/www/static/",
     #os.path.join(BASE_DIR,"static")
 ]
+
+
+
+AWS_ACCESS_KEY_ID = 'AKIATCKARPC47B42WIVI'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'trydatabase'
+AWS_S3_SIGNATURE_NAME = 's3v4',
+AWS_S3_REGION_NAME = 'eu-north-1'
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL =  None
+AWS_S3_VERITY = True
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
